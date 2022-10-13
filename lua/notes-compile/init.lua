6351953414df216ac67bca49829721856f066212
Omar Tejada <tejada.omar03@gmail.com>
2022-10-13 01:11:16 -0600

    feat: add toggleable autocmd that compiles on run
    Autocmd is in augroup 'notes-compile' and applies only to current buffer

--- lua/notes-compile/init.lua
+++ lua/notes-compile/init.lua
@@ -5,6 +5,27 @@ local processes = require('notes-compile.processes')
 local M = {}
 
 local setup_complete = false
+local autocmd_enabled = false
+
+M.toggle_autocmd = function (event)
+  if autocmd_enabled then
+    vim.api.nvim_del_augroup_by_name('notes-compile')
+    autocmd_enabled = false
+    return
+  end
+
+  vim.api.nvim_create_augroup('notes-compile', {})
+  vim.api.nvim_create_autocmd(event, {
+    group = 'notes-compile',
+    buffer = 0,
+    desc = 'Begin autocompilation of notes',
+    callback = function ()
+      M.compile()
+    end
+  })
+
+  autocmd_enabled = true
+end
 
 M.compile = function ()
   M.setup()
