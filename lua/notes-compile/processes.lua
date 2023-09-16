8a77c1f61cca54f63f7af6dee503421d26f3f474
Omar Tejada <tejada.omar03@gmail.com>
2023-09-15 18:17:14 -0600

    feat(zathura): make opening pid aware
    Allows for situations in which you may be working on 2 different
    documents in separate nvim instances
    
    E.g. `~/history/master.pdf` and `~/math/master.pdf`

--- lua/notes-compile/processes.lua
+++ lua/notes-compile/processes.lua
@@ -5,7 +5,7 @@ local M = {}
 local _compile_job = function(file_name, args, files)
   local pandoc_args = vim.list_extend({ '-o', file_name }, args)
   pandoc_args = vim.list_extend(pandoc_args, files)
----@diagnostic disable-next-line: missing-fields
+  ---@diagnostic disable-next-line: missing-fields
   return Job:new {
     command = 'pandoc',
     args = pandoc_args,
@@ -16,17 +16,26 @@ local _compile_job = function(file_name, args, files)
   }
 end
 
-local _is_zathura_running_job = function(file_name)
----@diagnostic disable-next-line: missing-fields
+local _is_zathura_running_job = function(file_name, pid)
+  ---@diagnostic disable-next-line: missing-fields
   return Job:new {
     command = 'pgrep',
-    args = { '-cfx', 'zathura ' .. file_name},
+    args = { '-P', pid, '-cfx', 'zathura ' .. file_name },
   }
+end
+
+local _get_current_pid = function()
+  local error_msg = '/proc/self/stat not found'
+  local file = assert(io.open('/proc/self/stat', 'r'), error_msg)
+  local stat_info = file:read('*l')
+  file:close()
+
+  return stat_info:match('^([%w]+)')
 end
 
 -- TODO: Look into customizing pdf viewer
 local _open_zathura_job = function(file_name)
----@diagnostic disable-next-line: missing-fields
+  ---@diagnostic disable-next-line: missing-fields
   return Job:new {
     command = 'zathura',
     args = { file_name },
@@ -37,7 +46,8 @@ end
 M.run = function(file_name, args, files, pandoc_integration)
   -- TODO: Create version that doesn't open zathura for BufWritePost
   local compile_job = _compile_job(file_name, args, files)
-  local is_zathura_running_job = _is_zathura_running_job(file_name)
+  local is_zathura_running_job =
+      _is_zathura_running_job(file_name, _get_current_pid())
   local open_zathura_job = _open_zathura_job(file_name)
 
   if pandoc_integration == true then
