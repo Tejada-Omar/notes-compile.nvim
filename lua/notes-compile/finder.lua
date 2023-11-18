c9fcb0495d78f166ef179d5da76973cde6ff2251
Omar Tejada <tejada.omar03@gmail.com>
2023-11-18 05:13:07 -0700

    Revert "refactor(finder): Use builtin plenary option"
    This reverts commit 41d2b217a32046ac1ff42a74544d449f7c9f64cd.
    
    Filtering through the file list *after* the fact is less computationally
    expensive then adding then removing an element each time an item is
    added using `scandir` (which is what would be done using `on_insert` and
    `table.remove`), especially since index order is not guaranteed an would
    require another search regardless

--- lua/notes-compile/finder.lua
+++ lua/notes-compile/finder.lua
@@ -6,23 +6,24 @@ M.find_files = function(skip)
   local scan = require('plenary.scandir')
   M.files = scan.scan_dir('.', {
     respect_gitignore = true,
-    search_pattern = function(f)
-      if skip == nil or vim.tbl_isempty(skip) then
-        if string.find(f, '.md', -3, true) == nil then return false end
-        return true
-      end
-
-      for _, entry in pairs(skip) do
-        if f == entry then
-          return false
-        elseif string.find(f, '.md', -3, true) == nil then
-          return false
-        end
-      end
-
-      return true
-    end,
   })
+
+  M.files = vim.tbl_filter(function(f)
+    if skip == nil or vim.tbl_isempty(skip) then
+      if string.find(f, '.md', -3, true) == nil then return false end
+      return true
+    end
+
+    for _, entry in pairs(skip) do
+      if f == entry then
+        return false
+      elseif string.find(f, '.md', -3, true) == nil then
+        return false
+      end
+    end
+
+    return true
+  end, M.files)
 
   table.sort(M.files, function(f1, f2)
     local function digit_len(num)
