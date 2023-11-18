41d2b217a32046ac1ff42a74544d449f7c9f64cd
Omar Tejada <tejada.omar03@gmail.com>
2023-11-18 02:03:27 -0700

    refactor(finder): Use builtin plenary option
    `tbl_filter` accomplishes the same goal as the builtin `search_pattern`
    option in `scandir`

--- lua/notes-compile/finder.lua
+++ lua/notes-compile/finder.lua
@@ -6,24 +6,23 @@ M.find_files = function(skip)
   local scan = require('plenary.scandir')
   M.files = scan.scan_dir('.', {
     respect_gitignore = true,
-  })
-
-  M.files = vim.tbl_filter(function(f)
-    if skip == nil or vim.tbl_isempty(skip) then
-      if string.find(f, '.md', -3, true) == nil then return false end
-      return true
-    end
-
-    for _, entry in pairs(skip) do
-      if f == entry then
-        return false
-      elseif string.find(f, '.md', -3, true) == nil then
-        return false
+    search_pattern = function(f)
+      if skip == nil or vim.tbl_isempty(skip) then
+        if string.find(f, '.md', -3, true) == nil then return false end
+        return true
       end
-    end
 
-    return true
-  end, M.files)
+      for _, entry in pairs(skip) do
+        if f == entry then
+          return false
+        elseif string.find(f, '.md', -3, true) == nil then
+          return false
+        end
+      end
+
+      return true
+    end,
+  })
 end
 
 return M
